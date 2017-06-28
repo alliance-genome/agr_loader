5ae53640fe504684d02127f6e3f83201659a98bd
Chris Tabone <ctabone@morgan.harvard.edu>
2017-06-28 18:32:49 -0400

    Updated gitignore and modified gene_indexer.

--- src/indexers/gene_indexer.py
+++ src/indexers/gene_indexer.py
@@ -2,15 +2,14 @@ from py2neo import Graph, Node, Relationship
 
 class GeneIndexer:
 
-	def __init__(self, graph):
-		self.graph = graph
-		self.statement = "MERGE (n:Gene)"
+    def __init__(self, graph):
+        self.graph = graph
 
-	def index_genes(self, data):
-		tx = self.graph.begin()
+    def index_genes(self, data):
+        tx = self.graph.begin()
 
-		for entry in data:
-			a = Node("Gene", primary_key=entry['primaryId'])
-			tx.merge(a, "Gene", "primary_key") # Merge on label "Gene" and primary_key of "primary_key".
+        for entry in data:
+            a = Node("Gene", primary_key=entry['primaryId'])
+            tx.merge(a, "Gene", "primary_key") # Merge on label "Gene" and primary_key of "primary_key".
 
-		tx.commit()
+        tx.commit()
