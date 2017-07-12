a8b915cef7cb3fbcb9fa9d97fb9b27a3342375ae
Christopher Tabone <christabone@gmail.com>
2017-07-12 10:18:24 -0400

    Last name fix.

--- src/fetch_index.py
+++ src/fetch_index.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     al.load_from_mods(test_set = test_set)
     #al.load_from_ontologies()
     #al.load_annotations()
-    al.create_indicies()
+    #al.create_indicies()
