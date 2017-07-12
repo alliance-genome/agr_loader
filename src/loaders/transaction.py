4e6819eecedcfe4becfe097ce953fd124987b184
Christopher Tabone <christabone@gmail.com>
2017-07-12 15:58:47 -0400

    Added specific connection variables.

--- src/loaders/transaction.py
+++ src/loaders/transaction.py
@@ -4,6 +4,7 @@ class Transaction():
 
     def __init__(self, graph):
         self.graph = graph
+        self.tracking_dict = {}
 
     def execute_transaction(self, query, data):
         with self.graph.session() as session:
@@ -27,9 +28,35 @@ class Transaction():
         Loads the BGI data into Neo4j.
         '''
         query = """
-            UNWIND $data as row \
-            MERGE (n:Gene {primary_key:row.primaryId})
-            SET n.symbol = row.symbol
+            UNWIND $data as row 
+            MERGE (n:Gene {primary_key:row.primaryId}) 
+            SET n.symbol = row.symbol 
+            SET n.taxonId = row.taxonId 
+            SET n.name = row.name 
+            SET n.description = row.description 
+            SET n.synonyms = row.synonyms
+            SET n.secondaryIds = row.secondaryIds
+            SET n.geneSynopsisUrl = row.geneSynopsisUrl
+            SET n.species = row.species
+            SET n.external_ids = row.external_ids
         """
-
         self.execute_transaction(query, data)
+
+        # "soTermId": geneRecord['soTermId'],
+        # "soTermName": None,
+        # "diseases": [],
+        # "external_ids": external_ids,
+        # "gene_biological_process": [],
+        # "gene_molecular_function": [],
+        # "gene_cellular_component": [],
+        # "genomeLocations": genomic_locations,
+        # "geneLiteratureUrl": geneRecord.get('geneLiteratureUrl'),
+        # "name_key": geneRecord['symbol'],
+        # "primaryId": primary_id,
+        # "crossReferences": crossReferences,
+        # "modCrossReference": modCrossReference,
+        # "category": "gene",
+        # "dateProduced": dateProduced,
+        # "dataProvider": dataProvider,
+        # "release": release,
+        # "href": None
