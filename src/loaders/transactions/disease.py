990a4aacfb77c0777d3e9da30363807a6696de81
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-08-01 14:50:15 -0700

    implement foreach with set in cypher query

--- src/loaders/transactions/disease.py
+++ src/loaders/transactions/disease.py
@@ -15,37 +15,32 @@ class DiseaseTransaction(Transaction):
         query = """
 
             UNWIND $data as row with row
+            MERGE (spec:Species {primaryId: row.taxonId})
 
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'gene' THEN [1] ELSE [] END |
                 MERGE (f:Gene:Gene {primaryKey:row.primaryId})
                 SET f.name = row.diseaseObjectName
-                MERGE (spec:Species {primaryId: row.taxonId})
-                SET spec.species = row.species
-                CREATE (g)-[:FROM_SPECIES]->(spec)
+                CREATE (f)-[:FROM_SPECIES]->(spec)
             )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'genotype' THEN [1] ELSE [] END |
                 MERGE (f:Genotype:Genotype {primaryKey:row.primaryId})
                 SET f.name = row.diseaseObjectName
-                SET spec.species = row.species
-                CREATE (g)-[:FROM_SPECIES]->(spec)
+                CREATE (f)-[:FROM_SPECIES]->(spec)
             )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'allele' THEN [1] ELSE [] END |
                 MERGE (f:Allele:Allele {primaryKey:row.primaryId})
                 SET f.name = row.diseaseObjectName
-                SET spec.species = row.species
-                CREATE (g)-[:FROM_SPECIES]->(spec)
+                CREATE (f)-[:FROM_SPECIES]->(spec)
             )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'transgene' THEN [1] ELSE [] END |
                 MERGE (f:Transgene:Transgene {primaryKey:row.primaryId})
                 SET f.name = row.diseaseObjectName
-                SET spec.species = row.species
-                CREATE (g)-[:FROM_SPECIES]->(spec)
+                CREATE (f)-[:FROM_SPECIES]->(spec)
             )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'fish' THEN [1] ELSE [] END |
                 MERGE (f:Fish:Fish {primaryKey:row.primaryId})
                 SET f.name = row.diseaseObjectName
-                SET spec.species = row.species
-                CREATE (g)-[:FROM_SPECIES]->(spec)
+                CREATE (f)-[:FROM_SPECIES]->(spec)
             )
 
         """
