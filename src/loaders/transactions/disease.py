380a7e8adbad246b39f2b351e673097e1da50fe7
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-08-01 08:27:12 -0700

    implement foreach with set in cypher query

--- src/loaders/transactions/disease.py
+++ src/loaders/transactions/disease.py
@@ -17,15 +17,21 @@ class DiseaseTransaction(Transaction):
             UNWIND $data as row with row
 
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'gene' THEN [1] ELSE [] END |
-                MERGE (g:Gene:Gene {primaryKey:row.primaryId}))
+                MERGE (g:Gene:Gene {primaryKey:row.primaryId})
+                set g.name = row.diseaseObjectName
+            )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'genotype' THEN [1] ELSE [] END |
-                MERGE (g:Genotype:Genotype {primaryKey:row.primaryId}))
+                MERGE (g:Genotype:Genotype {primaryKey:row.primaryId})
+                set g.name = row.diseaseObjectName)
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'allele' THEN [1] ELSE [] END |
-                MERGE (g:Allele:Allele {primaryKey:row.primaryId}))
+                MERGE (g:Allele:Allele {primaryKey:row.primaryId})
+                set g.name = row.diseaseObjectName)
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'transgene' THEN [1] ELSE [] END |
-                MERGE (g:Transgene:Transgene {primaryKey:row.primaryId}))
+                MERGE (g:Transgene:Transgene {primaryKey:row.primaryId})
+                set g.name = row.diseaseObjectName)
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'fish' THEN [1] ELSE [] END |
-                MERGE (g:Fish:Fish {primaryKey:row.primaryId}))
+                MERGE (g:Fish:Fish {primaryKey:row.primaryId})
+                set g.name = row.diseaseObjectName)
 
         """
         Transaction.execute_transaction(self, query, data)
