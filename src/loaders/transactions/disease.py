aa76e0d7570912a76ac0f5abd899682b3b21552e
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-08-10 09:40:59 -0700

    stash evidence

--- src/loaders/transactions/disease.py
+++ src/loaders/transactions/disease.py
@@ -17,11 +17,10 @@ class DiseaseTransaction(Transaction):
         query = """
 
             UNWIND $data as row
-           // UNWIND row.ecode as codes
 
 
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'gene' THEN [1] ELSE [] END |
-
+                //TODO: test if adding "DiseaseObject" label breaks merge
                 MERGE (f:Gene {primaryKey:row.primaryId})
 
                 MERGE (f)-[:FROM_SPECIES]->(spec)
@@ -62,20 +61,16 @@ class DiseaseTransaction(Transaction):
                 SET pub.pubModUrl = row.pubModUrl
                 SET pub.pubMedUrl = row.pubMedUrl
 
-                MERGE (e:Evidence {primaryKey:row.diseaseEvidenceCodePubAssociationId, link_from:row.pubPrimaryKey, link_to:row.diseaseAssociationId})
                 MERGE (da)-[dapa:ANNOTATED]->(pub)
-                MERGE (da)-[dae:ANNOTATED]->(e)
-                MERGE (pub)-[pubEv:ANNOTATED]->(e)
 
                 MERGE (ecs:EvidenceCodes {evidenceCodes:row.evidenceCodes})
                 //Create Association nodes for other identifiers.
-                MERGE (ecs)-[ecda:ANNOTATED]->(e)
-
+                MERGE (ecs)-[ecda:ANNOTATED]->(da)
             )
 
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'genotype' THEN [1] ELSE [] END |
 
-                MERGE (f:Genotype {primaryKey:row.primaryId})
+                MERGE (f:Genotype:DiseaseObject {primaryKey:row.primaryId})
 
                 MERGE (f)-[:FROM_SPECIES]->(spec)
                 SET f.with = row.with
@@ -91,7 +86,7 @@ class DiseaseTransaction(Transaction):
                     MERGE (f)-[fq:IS_NOT_MODEL_OF]->(d))
 
                 //Create the Association node to be used for the object/doTerm
-                MERGE (gda:DiseaseAssociation {primaryKey:row.diseaseAssociationId, link_from:row.primaryId, link_to:row.doId})
+                MERGE (gda:Association {primaryKey:row.diseaseAssociationId, link_from:row.primaryId, link_to:row.doId})
 
                 //Create the relationship from the object node to association node.
                 //Create the relationship from the association node to the DoTerm node.
@@ -107,14 +102,11 @@ class DiseaseTransaction(Transaction):
                 SET pubg.pubModUrl = row.pubModUrl
                 SET pubg.pubMedUrl = row.pubMedUrl
 
-                MERGE (e:Evidence {primaryKey:row.diseaseEvidenceCodePubAssociationId, link_from:row.pubPrimaryKey, link_to:row.diseaseAssociationId})
-                MERGE (gda)-[dapa:ANNOTATED]->(pubg)
-                MERGE (gda)-[dae:ANNOTATED]->(e)
-                MERGE (pubg)-[pubEv:ANNOTATED]->(e)
+                MERGE (da)-[dapa:ANNOTATED]->(pub)
 
                 MERGE (ecs:EvidenceCodes {evidenceCodes:row.evidenceCodes})
                 //Create Association nodes for other identifiers.
-                MERGE (ecs)-[ecda:ANNOTATED]->(e)
+                MERGE (ecs)-[ecda:ANNOTATED]->(da)
 
             )
             FOREACH (x IN CASE WHEN row.diseaseObjectType = 'allele' THEN [1] ELSE [] END |
@@ -147,14 +139,11 @@ class DiseaseTransaction(Transaction):
                 SET puba.pubModUrl = row.pubModUrl
                 SET puba.pubMedUrl = row.pubMedUrl
 
-                MERGE (e:Evidence {primaryKey:row.diseaseEvidenceCodePubAssociationId, link_from:row.pubPrimaryKey, link_to:row.diseaseAssociationId})
-                MERGE (ada)-[dapa:ANNOTATED]->(puba)
-                MERGE (ada)-[dae:ANNOTATED]->(e)
-                MERGE (puba)-[pubEv:ANNOTATED]->(e)
+                MERGE (da)-[dapa:ANNOTATED]->(pub)
 
                 MERGE (ecs:EvidenceCodes {evidenceCodes:row.evidenceCodes})
                 //Create Association nodes for other identifiers.
-                MERGE (ecs)-[ecda:ANNOTATED]->(e)
+                MERGE (ecs)-[ecda:ANNOTATED]->(da)
 
                 FOREACH (rel IN CASE when row.relationshipType = 'is_marker_for' THEN [1] ELSE [] END |
                     MERGE (f)-[fa:IS_MARKER_FOR]->(d))
@@ -201,9 +190,12 @@ class DiseaseTransaction(Transaction):
                 SET tpub.pubModUrl = row.pubModUrl
                 SET tpub.pubMedUrl = row.pubMedUrl
 
-                MERGE (tda)-[dapa:ANNOTATED]->(tpub)
-                MERGE (tda)-[dae:ANNOTATED]->(e)
-                MERGE (tpub)-[tpubEv:ANNOTATED]->(e)
+                MERGE (da)-[dapa:ANNOTATED]->(pub)
+
+                MERGE (ecs:EvidenceCodes {evidenceCodes:row.evidenceCodes})
+                //Create Association nodes for other identifiers.
+                MERGE (ecs)-[ecda:ANNOTATED]->(da)
+
 
                 FOREACH (rel IN CASE when row.relationshipType = 'is_marker_for' THEN [1] ELSE [] END |
                     MERGE (f)-[fa:IS_MARKER_FOR]->(d))
@@ -252,21 +244,17 @@ class DiseaseTransaction(Transaction):
                 MERGE (fida)-[dae:ANNOTATED]->(e)
                 MERGE (fpub)-[fpubEv:ANNOTATED]->(e)
 
+                MERGE (da)-[dapa:ANNOTATED]->(pub)
+
                 MERGE (ecs:EvidenceCodes {evidenceCodes:row.evidenceCodes})
                 //Create Association nodes for other identifiers.
-                MERGE (ecs)-[ecda:ANNOTATED]->(e)
-
-
+                MERGE (ecs)-[ecda:ANNOTATED]->(da)
 
                 FOREACH (rel IN CASE when row.relationshipType = 'is_model_of' THEN [1] ELSE [] END |
                     MERGE (f)-[fa:IS_MODEL_OF]->(d))
                 FOREACH (qualifier IN CASE when row.qualifier = 'NOT' and row.relationshipType = 'is_model_of' THEN [1] ELSE [] END |
                     MERGE (f)-[fq:IS_NOT_MODEL_OF]->(d))
             )
-
-
-           // FOREACH (code in codes |
-            //        MERGE (ec:EvidenceCode {primaryKey:code.code}))
 
 
         """
