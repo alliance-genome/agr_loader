491dbc2c894a62506ba8d5737dbf07229808dc5d
Chris Tabone <ctabone@morgan.harvard.edu>
2017-08-30 14:23:43 -0400

    Removed old OMIM module reference.

--- src/extractors/__init__.py
+++ src/extractors/__init__.py
@@ -3,7 +3,6 @@ from .go_ext import GOExt
 from .do_ext import DOExt
 from .so_ext import SOExt
 from .ortho_ext import OrthoExt
-from .omim_ext import OMIMExt
 from .bgi_ext import BGIExt
 from .go_gene_annot_ext import GOGeneAnnot
 from .disease_ext import DiseaseExt
