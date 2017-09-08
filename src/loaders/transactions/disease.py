514a7e4b0dadef40545e1ccf9f8c7566eb85ebc7
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-09-08 08:06:34 -0700

    fixing disease dups

--- src/loaders/transactions/disease.py
+++ src/loaders/transactions/disease.py
@@ -12,53 +12,50 @@ class DiseaseTransaction(Transaction):
         executeGene = """
             UNWIND $data as row
 
+            MATCH(f:Gene {primaryKey:row.primaryId})
+                //SET f :DiseaseObject
+
             MERGE (d:DOTerm:Ontology {primaryKey:row.doId})
                SET d.doDisplayId = row.doDisplayId
                SET d.doUrl = row.doUrl
                SET d.doPrefix = row.doPrefix
                SET d.doId = row.doId
 
-            MERGE (f:Gene {primaryKey:row.primaryId})
-                SET f :DiseaseObject
+                MERGE (spec:Species {primaryKey: row.taxonId})
+                MERGE (f)<-[:FROM_SPECIES]->(spec)
+                    //SET f.with = row.with
 
-            MERGE (spec:Species {primaryKey: row.taxonId})
-            MERGE (f)<-[:FROM_SPECIES]->(spec)
-                SET f.with = row.with
+                MERGE (da:Association {primaryKey:row.uuid})
+                    SET da :DiseaseGeneJoin
 
-            //the foreach statments that represent relationships/associationType are not here for capitalization purposes.
-            //instead we mimic a conditional statement by creating a collection with 1 value, and an empty collection.
-            //Create the Association node to be used for the object/doTerm
-            MERGE (da:Association {primaryKey:row.uuid})
-                SET da :DiseaseGeneJoin
+                FOREACH (rel IN CASE when row.relationshipType = 'is_marker_for' THEN [1] ELSE [] END |
+                    MERGE (f)<-[fa:IS_MARKER_FOR {uuid:row.uuid}]->(d))
+                    SET da.joinType = 'is_marker_of'
 
-            FOREACH (rel IN CASE when row.relationshipType = 'is_marker_for' THEN [1] ELSE [] END |
-                MERGE (f)<-[fa:IS_MARKER_FOR {uuid:row.uuid}]->(d))
-                SET da.joinType = 'is_marker_of'
+                FOREACH (rel IN CASE when row.relationshipType = 'is_implicated_in' THEN [1] ELSE [] END |
+                    MERGE (f)<-[fa:IS_IMPLICATED_IN {uuid:row.uuid}]->(d))
+                    SET da.joinType = 'is_implicated_in'
 
-            FOREACH (rel IN CASE when row.relationshipType = 'is_implicated_in' THEN [1] ELSE [] END |
-                MERGE (f)<-[fa:IS_IMPLICATED_IN {uuid:row.uuid}]->(d))
-                SET da.joinType = 'is_implicated_in'
+                //Create the relationship from the object node to association node.
+                //Create the relationship from the association node to the DoTerm node.
 
-            //Create the relationship from the object node to association node.
-            //Create the relationship from the association node to the DoTerm node.
+                MERGE (f)-[fda:ASSOCIATION]->(da)
+                MERGE (da)-[dad:ASSOCIATION]->(d)
 
-            MERGE (f)-[fda:ASSOCIATION]->(da)
-            MERGE (da)-[dad:ASSOCIATION]->(d)
+                // Publications
 
-            // Publications
+                MERGE (pub:Publication {primaryKey:row.pubPrimaryKey})
+                    SET pub.pubModId = row.pubModId
+                    SET pub.pubMedId = row.pubMedId
+                    SET pub.pubModUrl = row.pubModUrl
+                    SET pub.pubMedUrl = row.pubMedUrl
 
-            MERGE (pub:Publication {primaryKey:row.pubPrimaryKey})
-                SET pub.pubModId = row.pubModId
-                SET pub.pubMedId = row.pubMedId
-                SET pub.pubModUrl = row.pubModUrl
-                SET pub.pubMedUrl = row.pubMedUrl
+                MERGE (da)-[dapu:EVIDENCE]->(pub)
 
-            MERGE (da)-[dapu:EVIDENCE]->(pub)
-
-            FOREACH (entity in row.ecodes|
-                    MERGE (ecode1:EvidenceCode {primaryKey:entity})
-                    MERGE (da)-[daecode1:EVIDENCE]->(ecode1)
-            )
+                FOREACH (entity in row.ecodes|
+                        MERGE (ecode1:EvidenceCode {primaryKey:entity})
+                        MERGE (da)-[daecode1:EVIDENCE]->(ecode1)
+                )
 
             """
 
