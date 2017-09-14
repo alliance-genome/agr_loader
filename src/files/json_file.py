62d42c06e9e479cddb3e0a13aa6e30f69c17a7a7
Chris Tabone <ctabone@morgan.harvard.edu>
2017-09-14 12:20:57 -0400

    Added JSON validation for bgi, disease, and ortho.

--- src/files/json_file.py
+++ src/files/json_file.py
@@ -1,11 +1,46 @@
 import json
 import codecs
+import jsonschema as js
+import sys
+import os
 
 class JSONFile(object):
 
-    def get_data(self, filename):
+    def get_data(self, filename, jsonType):
         print("Loading json data from %s ..." % (filename))
         with codecs.open(filename, 'r', 'utf-8') as f:
             data = json.load(f)
         f.close()
+        self.validate_json(data, filename, jsonType)
         return data
+
+    def validate_json(self, data, filename, jsonType):
+        print("Validating %s JSON." % (jsonType))
+
+        schema_file_name = None
+        if jsonType == 'disease':
+            schema_file_name = 'schemas/disease/diseaseMetaDataDefinition.json'
+        elif jsonType == 'BGI':
+            schema_file_name = 'schemas/basicGeneInfoFile.json'
+        elif jsonType == 'orthology':
+            schema_file_name = 'schemas/orthology/orthoHeader.json'
+
+        with open(schema_file_name) as schema_file:
+            schema = json.load(schema_file)
+
+        # Defining a resolver for relative paths and schema issues, see https://github.com/Julian/jsonschema/issues/313
+        # and https://github.com/Julian/jsonschema/issues/274
+        sSchemaDir = os.path.dirname(os.path.abspath(schema_file_name))
+        oResolver = js.RefResolver(base_uri = 'file://' + sSchemaDir + '/', referrer = schema)
+
+        try:
+            js.validate(data, schema, format_checker=js.FormatChecker(), resolver=oResolver)
+            print("'%s' successfully validated against '%s'" % (filename, schema_file_name))
+        except js.ValidationError as e:
+            print(e.message)
+            print(e)
+            raise SystemExit("FATAL ERROR in JSON validation.")
+        except js.SchemaError as e:
+            print(e.message)
+            print(e)
+            raise SystemExit("FATAL ERROR in JSON validation.")      
