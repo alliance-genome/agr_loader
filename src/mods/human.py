42ffb6efebd5946446f449b7263031435ece496e
Chris Tabone <ctabone@morgan.harvard.edu>
2017-09-18 16:03:23 -0400

    Updated s3 location for human.

--- src/mods/human.py
+++ src/mods/human.py
@@ -7,7 +7,7 @@ class Human(MOD):
         self.loadFile = "RGD_1.0.1.tar.gz"
         self.bgiName = "/RGD_1.0.1_basicGeneInformation.9606.json"
         self.diseaseName = "/RGD_1.0.1_disease.9606.daf.json"
-        self.geneAssociationFile = "/GO/ANNOT/gene_association.human.gz"
+        self.geneAssociationFile = "gene_association.human.gz"
         self.identifierPrefix = "" # None for Human.
 
     def load_genes(self, batch_size, testObject):
