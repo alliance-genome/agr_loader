2c8aedd4161ed33f8ac506f9210c48e0e2f5362a
Chris Tabone <ctabone@morgan.harvard.edu>
2017-09-19 18:28:39 -0400

    Added more tests for schema.

--- src/test/schema_tests.py
+++ src/test/schema_tests.py
@@ -25,7 +25,23 @@ identifierRequiredProperties = ['primaryKey']
 
 synonymRequiredProperties = ['primaryKey', 'name']
 
-crossReferenceRequiredProperties = ['localId', 'name', 'crossRefCompleteUrl', 'primaryKey', 'prefix']
+crossReferenceRequiredProperties = ['localId', 'name', 'primaryKey', 'prefix']
+
+speciesRequiredProperties = ['name', 'species', 'primaryKey']
+
+entityRequiredProperties = ['dateProduced', 'primaryKey']
+
+chromosomeRequiredProperties = ['primaryKey']
+
+diseaseGeneJoinRequiredProperties = ['joinType', 'primaryKey']
+
+associationRequiredProperties = ['joinType', 'primaryKey']
+
+publicationRequiredProperties = ['pubMedId', 'pubModId', 'pubModUrl', 'primaryKey']
+
+evidenceCodeRequiredProperties = ['primaryKey']
+
+gotermRequiredProperties = ['primaryKey']
 
 def execute_transaction(query):
     host = os.environ['NEO4J_NQC_HOST']
@@ -139,6 +155,134 @@ def test_cross_reference_properties(data):
 @pytest.mark.parametrize("data", crossReferenceRequiredProperties)
 def test_cross_reference_properties_not_null(data):
     query = 'MATCH (n:CrossReference) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", speciesRequiredProperties)
+def test_species_properties(data):
+    query = 'MATCH (n:Species) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", speciesRequiredProperties)
+def test_species_properties_not_null(data):
+    query = 'MATCH (n:Species) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", entityRequiredProperties)
+def test_entity_properties(data):
+    query = 'MATCH (n:Entity) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", entityRequiredProperties)
+def test_entity_properties_not_null(data):
+    query = 'MATCH (n:Entity) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", chromosomeRequiredProperties)
+def test_chromosome_properties(data):
+    query = 'MATCH (n:Chromosome) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", chromosomeRequiredProperties)
+def test_chromosome_properties_not_null(data):
+    query = 'MATCH (n:Chromosome) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", diseaseGeneJoinRequiredProperties)
+def test_disease_gene_join_properties(data):
+    query = 'MATCH (n:DiseaseGeneJoin) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", diseaseGeneJoinRequiredProperties)
+def test_disease_gene_join_properties_not_null(data):
+    query = 'MATCH (n:DiseaseGeneJoin) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", associationRequiredProperties)
+def test_association_properties(data):
+    query = 'MATCH (n:Association) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", associationRequiredProperties)
+def test_association_properties_not_null(data):
+    query = 'MATCH (n:Association) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", publicationRequiredProperties)
+def test_publication_properties(data):
+    query = 'MATCH (n:Publication) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", publicationRequiredProperties)
+def test_publication_not_null(data):
+    query = 'MATCH (n:Publication) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", evidenceCodeRequiredProperties)
+def test_evidence_code_properties(data):
+    query = 'MATCH (n:EvidenceCode) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", evidenceCodeRequiredProperties)
+def test_evidence_code_not_null(data):
+    query = 'MATCH (n:EvidenceCode) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", gotermRequiredProperties)
+def test_goterm_properties(data):
+    query = 'MATCH (n:GOTerm) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+
+    result = execute_transaction(query)
+    for record in result:
+        assert record["count"] == 0
+
+@pytest.mark.parametrize("data", gotermRequiredProperties)
+def test_goterm_not_null(data):
+    query = 'MATCH (n:GOTerm) WHERE n.%s is NULL RETURN count(n) as count' % (data)
 
     result = execute_transaction(query)
     for record in result:
