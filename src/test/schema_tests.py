c0680a9f7fd9178804a7682b4f1e0bc8d5b9d7c3
Chris Tabone <ctabone@morgan.harvard.edu>
2017-09-20 12:13:32 -0400

    Restructured functions for schema tests.

--- src/test/schema_tests.py
+++ src/test/schema_tests.py
@@ -2,47 +2,6 @@ from neo4j.v1 import GraphDatabase
 import os
 import pytest
 
-# Query to return all distinct node labels:
-# MATCH (n) RETURN DISTINCT labels(n)
-
-nodeLabels = ['Ontology', 'SOTerm', 'DOTerm', 'Identifier', 'Gene', 'Synonym', 'CrossReference', \
-              'Species', 'Entity', 'Chromosome', 'DiseaseGeneJoin', 'Association', 'Publication', \
-              'EvidenceCode', 'GOTerm']
-
-# Query to return all distinct properties from all nodes of a certain type:
-# MATCH (n:Gene) WITH DISTINCT keys(n) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;
-
-geneRequiredProperties = ['modGlobalCrossRefId', 'dateProduced', 'geneLiteratureUrl', 'dataProvider',  \
-                  'modCrossRefCompleteUrl', 'taxonId', 'geneticEntityExternalUrl', 'modLocalId', \
-                  'symbol', 'name', 'primaryKey', 'modGlobalId']
-
-sotermRequiredProperties = ['name', 'primaryKey']
-
-dotermRequiredProperties = ['doPrefix', 'doId', 'doDisplayId', 'doUrl', 'defLinks', 'is_obsolete', 'subset', \
-                           'name', 'nameKey', 'primaryKey', 'definition']
-
-identifierRequiredProperties = ['primaryKey']
-
-synonymRequiredProperties = ['primaryKey', 'name']
-
-crossReferenceRequiredProperties = ['localId', 'name', 'primaryKey', 'prefix']
-
-speciesRequiredProperties = ['name', 'species', 'primaryKey']
-
-entityRequiredProperties = ['dateProduced', 'primaryKey']
-
-chromosomeRequiredProperties = ['primaryKey']
-
-diseaseGeneJoinRequiredProperties = ['joinType', 'primaryKey']
-
-associationRequiredProperties = ['joinType', 'primaryKey']
-
-publicationRequiredProperties = ['pubMedId', 'pubModId', 'pubModUrl', 'primaryKey']
-
-evidenceCodeRequiredProperties = ['primaryKey']
-
-gotermRequiredProperties = ['primaryKey']
-
 def execute_transaction(query):
     host = os.environ['NEO4J_NQC_HOST']
     port = os.environ['NEO4J_NQC_PORT']
@@ -56,234 +15,177 @@ def execute_transaction(query):
 
     return result    
 
-@pytest.mark.parametrize("data", nodeLabels)
-def test_node_labels(data):
-    query = 'MATCH (n:%s) RETURN distinct count(n) as count' % (data)
+def pytest_generate_tests(metafunc):
+    # called once per each test function
+    funcarglist = metafunc.cls.params[metafunc.function.__name__]
+    argnames = sorted(funcarglist[0])
+    metafunc.parametrize(argnames, [[funcargs[name] for name in argnames]
+            for funcargs in funcarglist])
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] > 0
+class TestClass(object):
+    # a map specifying multiple argument sets for a test method
+    params = {
+        'test_node_exists': [dict(node='Ontology'), \
+                            dict(node='SOTerm'), \
+                            dict(node='DOTerm'), \
+                            dict(node='GOTerm'), \
+                            dict(node='Identifier'), \
+                            dict(node='Gene'), \
+                            dict(node='Synonym'), \
+                            dict(node='CrossReference'), \
+                            dict(node='Species'), \
+                            dict(node='Entity'), \
+                            dict(node='Chromosome'), \
+                            dict(node='DiseaseGeneJoin'), \
+                            dict(node='Association'), \
+                            dict(node='Publication'), \
+                            dict(node='EvidenceCode')],
 
-@pytest.mark.parametrize("data", geneRequiredProperties)
-def test_gene_properties(data):
-    query = 'MATCH (n:Gene) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+        'test_prop_exist': [dict(node='Gene', prop='modGlobalCrossRefId'), \
+                            dict(node='Gene', prop='dateProduced'), \
+                            dict(node='Gene', prop='geneLiteratureUrl'), \
+                            dict(node='Gene', prop='dataProvider'), \
+                            dict(node='Gene', prop='modCrossRefCompleteUrl'), \
+                            dict(node='Gene', prop='taxonId'), \
+                            dict(node='Gene', prop='geneticEntityExternalUrl'), \
+                            dict(node='Gene', prop='modLocalId'), \
+                            dict(node='Gene', prop='symbol'), \
+                            dict(node='Gene', prop='name'), \
+                            dict(node='Gene', prop='primaryKey'), \
+                            dict(node='Gene', prop='modGlobalId'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='GOTerm', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", geneRequiredProperties)
-def test_gene_properties_not_null(data):
-    query = 'MATCH (n:Gene) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='SOTerm', prop='name'), \
+                            dict(node='SOTerm', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='DOTerm', prop='doPrefix'), \
+                            dict(node='DOTerm', prop='doId'), \
+                            dict(node='DOTerm', prop='doDisplayId'), \
+                            dict(node='DOTerm', prop='doUrl'), \
+                            dict(node='DOTerm', prop='defLinks'), \
+                            dict(node='DOTerm', prop='is_obsolete'), \
+                            dict(node='DOTerm', prop='subset'), \
+                            dict(node='DOTerm', prop='name'), \
+                            dict(node='DOTerm', prop='nameKey'), \
+                            dict(node='DOTerm', prop='primaryKey'), \
+                            dict(node='DOTerm', prop='definition'), \
 
-@pytest.mark.parametrize("data", sotermRequiredProperties)
-def test_soterm_properties(data):
-    query = 'MATCH (n:SOTerm) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='Identifier', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Synonym', prop='primaryKey'), \
+                            dict(node='Synonym', prop='name'), \
 
-@pytest.mark.parametrize("data", sotermRequiredProperties)
-def test_soterm_properties_not_null(data):
-    query = 'MATCH (n:SOTerm) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='CrossReference', prop='localId'), \
+                            dict(node='CrossReference', prop='name'), \
+                            dict(node='CrossReference', prop='primaryKey'), \
+                            dict(node='CrossReference', prop='prefix'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Species', prop='name'), \
+                            dict(node='Species', prop='species'), \
+                            dict(node='Species', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", dotermRequiredProperties)
-def test_doterm_properties(data):
-    query = 'MATCH (n:DOTerm) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='Entity', prop='dateProduced'), \
+                            dict(node='Entity', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Chromosome', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", dotermRequiredProperties)
-def test_doterm_properties_not_null(data):
-    query = 'MATCH (n:DOTerm) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='DiseaseGeneJoin', prop='joinType'), \
+                            dict(node='DiseaseGeneJoin', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Association', prop='joinType'), \
+                            dict(node='Association', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", identifierRequiredProperties)
-def test_identifier_properties(data):
-    query = 'MATCH (n:Identifier) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='Publication', prop='pubMedId'), \
+                            dict(node='Publication', prop='pubModId'), \
+                            dict(node='Publication', prop='pubModUrl'), \
+                            dict(node='Publication', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='EvidenceCode', prop='primaryKey')],
 
-@pytest.mark.parametrize("data", identifierRequiredProperties)
-def test_identifier_properties_not_null(data):
-    query = 'MATCH (n:Identifier) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+        'test_prop_not_null': [dict(node='Gene', prop='modGlobalCrossRefId'), \
+                            dict(node='Gene', prop='dateProduced'), \
+                            dict(node='Gene', prop='geneLiteratureUrl'), \
+                            dict(node='Gene', prop='dataProvider'), \
+                            dict(node='Gene', prop='modCrossRefCompleteUrl'), \
+                            dict(node='Gene', prop='taxonId'), \
+                            dict(node='Gene', prop='geneticEntityExternalUrl'), \
+                            dict(node='Gene', prop='modLocalId'), \
+                            dict(node='Gene', prop='symbol'), \
+                            dict(node='Gene', prop='name'), \
+                            dict(node='Gene', prop='primaryKey'), \
+                            dict(node='Gene', prop='modGlobalId'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='GOTerm', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", synonymRequiredProperties)
-def test_synonym_properties(data):
-    query = 'MATCH (n:synonym) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='SOTerm', prop='name'), \
+                            dict(node='SOTerm', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='DOTerm', prop='doPrefix'), \
+                            dict(node='DOTerm', prop='doId'), \
+                            dict(node='DOTerm', prop='doDisplayId'), \
+                            dict(node='DOTerm', prop='doUrl'), \
+                            dict(node='DOTerm', prop='defLinks'), \
+                            dict(node='DOTerm', prop='is_obsolete'), \
+                            dict(node='DOTerm', prop='subset'), \
+                            dict(node='DOTerm', prop='name'), \
+                            dict(node='DOTerm', prop='nameKey'), \
+                            dict(node='DOTerm', prop='primaryKey'), \
+                            dict(node='DOTerm', prop='definition'), \
 
-@pytest.mark.parametrize("data", synonymRequiredProperties)
-def test_synonym_properties_not_null(data):
-    query = 'MATCH (n:synonym) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='Identifier', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Synonym', prop='primaryKey'), \
+                            dict(node='Synonym', prop='name'), \
 
-@pytest.mark.parametrize("data", crossReferenceRequiredProperties)
-def test_cross_reference_properties(data):
-    query = 'MATCH (n:CrossReference) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='CrossReference', prop='localId'), \
+                            dict(node='CrossReference', prop='name'), \
+                            dict(node='CrossReference', prop='primaryKey'), \
+                            dict(node='CrossReference', prop='prefix'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Species', prop='name'), \
+                            dict(node='Species', prop='species'), \
+                            dict(node='Species', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", crossReferenceRequiredProperties)
-def test_cross_reference_properties_not_null(data):
-    query = 'MATCH (n:CrossReference) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='Entity', prop='dateProduced'), \
+                            dict(node='Entity', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Chromosome', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", speciesRequiredProperties)
-def test_species_properties(data):
-    query = 'MATCH (n:Species) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+                            dict(node='DiseaseGeneJoin', prop='joinType'), \
+                            dict(node='DiseaseGeneJoin', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='Association', prop='joinType'), \
+                            dict(node='Association', prop='primaryKey'), \
 
-@pytest.mark.parametrize("data", speciesRequiredProperties)
-def test_species_properties_not_null(data):
-    query = 'MATCH (n:Species) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+                            dict(node='Publication', prop='pubMedId'), \
+                            dict(node='Publication', prop='pubModId'), \
+                            dict(node='Publication', prop='pubModUrl'), \
+                            dict(node='Publication', prop='primaryKey'), \
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+                            dict(node='EvidenceCode', prop='primaryKey')]
+    }
 
-@pytest.mark.parametrize("data", entityRequiredProperties)
-def test_entity_properties(data):
-    query = 'MATCH (n:Entity) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+    # Query to return all distinct properties from all nodes of a certain type:
+    # MATCH (n:Gene) WITH DISTINCT keys(n) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+    def test_node_exists(self, node):
+        query = 'MATCH (n:%s) RETURN distinct count(n) as count' % (node)
 
-@pytest.mark.parametrize("data", entityRequiredProperties)
-def test_entity_properties_not_null(data):
-    query = 'MATCH (n:Entity) WHERE n.%s is NULL RETURN count(n) as count' % (data)
+        result = execute_transaction(query)
+        for record in result:
+            assert record["count"] > 0
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+    def test_prop_exist(self, node, prop):
+        query = 'MATCH (n:%s) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (node, prop)
 
-@pytest.mark.parametrize("data", chromosomeRequiredProperties)
-def test_chromosome_properties(data):
-    query = 'MATCH (n:Chromosome) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
+        result = execute_transaction(query)
+        for record in result:
+            assert record["count"] == 0
 
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+    def test_prop_not_null(self, node, prop):
+        query = 'MATCH (n:%s) WHERE n.%s is NULL RETURN count(n) as count' % (node, prop)
 
-@pytest.mark.parametrize("data", chromosomeRequiredProperties)
-def test_chromosome_properties_not_null(data):
-    query = 'MATCH (n:Chromosome) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", diseaseGeneJoinRequiredProperties)
-def test_disease_gene_join_properties(data):
-    query = 'MATCH (n:DiseaseGeneJoin) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", diseaseGeneJoinRequiredProperties)
-def test_disease_gene_join_properties_not_null(data):
-    query = 'MATCH (n:DiseaseGeneJoin) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", associationRequiredProperties)
-def test_association_properties(data):
-    query = 'MATCH (n:Association) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", associationRequiredProperties)
-def test_association_properties_not_null(data):
-    query = 'MATCH (n:Association) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", publicationRequiredProperties)
-def test_publication_properties(data):
-    query = 'MATCH (n:Publication) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", publicationRequiredProperties)
-def test_publication_not_null(data):
-    query = 'MATCH (n:Publication) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", evidenceCodeRequiredProperties)
-def test_evidence_code_properties(data):
-    query = 'MATCH (n:EvidenceCode) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", evidenceCodeRequiredProperties)
-def test_evidence_code_not_null(data):
-    query = 'MATCH (n:EvidenceCode) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", gotermRequiredProperties)
-def test_goterm_properties(data):
-    query = 'MATCH (n:GOTerm) WHERE NOT EXISTS(n.%s) RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
-
-@pytest.mark.parametrize("data", gotermRequiredProperties)
-def test_goterm_not_null(data):
-    query = 'MATCH (n:GOTerm) WHERE n.%s is NULL RETURN count(n) as count' % (data)
-
-    result = execute_transaction(query)
-    for record in result:
-        assert record["count"] == 0
+        result = execute_transaction(query)
+        for record in result:
+            assert record["count"] == 0
