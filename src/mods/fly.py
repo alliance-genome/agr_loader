4ae103c40e411d1b68e5afddc65d7431c93066ee
Chris Tabone <christabone@gmail.com>
2018-01-11 17:20:34 -0500

    Update fly.py
    Incremented the FB tar.gz to account for allele and gene disease file.

--- src/mods/fly.py
+++ src/mods/fly.py
@@ -4,7 +4,7 @@ class FlyBase(MOD):
 
     def __init__(self):
         self.species = "Drosophila melanogaster"
-        self.loadFile = "FB_1.0.4_1.tar.gz"
+        self.loadFile = "FB_1.0.4_2.tar.gz"
         self.bgiName = "/FB_1.0.4_BGI.json"
         self.diseaseName = "/FB_1.0.4_disease.json"
         self.alleleName = "/FB_1.0.4_feature.json"
