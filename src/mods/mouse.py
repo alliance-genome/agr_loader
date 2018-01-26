408bf7a383206a5f3d39ede6a592cfe9fffe8587
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-01-26 15:33:33 -0800

    some refactoring, added enum, added data dump update

--- src/mods/mouse.py
+++ src/mods/mouse.py
@@ -4,7 +4,7 @@ class MGI(MOD):
 
     def __init__(self):
         self.species = "Mus musculus"
-        self.loadFile = "MGI_1.0.4_1.tar.gz"
+        self.loadFile = "MGI_1.0.4_2.tar.gz"
         self.bgiName = "/MGI_1.0.4_BGI.json"
         self.alleleName = "/MGI_1.0.4_feature.json"
         self.diseaseName = "/MGI_1.0.4_disease.json"
