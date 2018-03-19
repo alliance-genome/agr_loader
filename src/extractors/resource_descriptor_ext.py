c36e7477da666cbcb0da70058145615a6d589a4c
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-03-19 12:01:26 -0700

    incorporated yaml into aggregate loader.

--- src/extractors/resource_descriptor_ext.py
+++ src/extractors/resource_descriptor_ext.py
@@ -11,15 +11,15 @@ class ResourceDescriptor():
         self.filename = "resourceDescriptors.yaml"
 
     def get_data(self):
-        # if not os.path.exists(self.savepath):
-        #     print("Making temp file storage: " + self.savepath)
-        #     os.makedirs(self.savepath)
-        # url = "https://github.com/alliance-genome/agr_schemas/blob/master/" + self.filename
-        # if not os.path.exists(self.savepath + "/" + self.filename):
-        #     with urllib.request.urlopen(url) as response, open(self.savepath + "/" + self.filename, 'wb') as outfile:
-        #         shutil.copyfileobj(response, outfile)
-        # else:
-        #     print("File: " + self.savepath + "/" + self.filename + " already exists not downloading")
+        if not os.path.exists(self.savepath):
+            print("Making temp file storage: " + self.savepath)
+            os.makedirs(self.savepath)
+        url = "https://github.com/alliance-genome/agr_schemas/blob/develop/" + self.filename
+        if not os.path.exists(self.savepath + "/" + self.filename):
+            with urllib.request.urlopen(url) as response, open(self.savepath + "/" + self.filename, 'wb') as outfile:
+                shutil.copyfileobj(response, outfile)
+        else:
+            print("File: " + self.savepath + "/" + self.filename + " already exists not downloading")
 
         with codecs.open(self.savepath + "/" + self.filename, 'r', 'utf-8') as stream:
             try:
