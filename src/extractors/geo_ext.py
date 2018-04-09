c0c9c4fbea5ba2eb7c949debfbb0e459b86cdc64
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-04-09 08:45:58 -0700

    parse out entrez ids from json

--- src/extractors/geo_ext.py
+++ src/extractors/geo_ext.py
@@ -10,10 +10,13 @@ class GeoExt(object):
         url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?term=gene_geoprofiles%5Bfilter%5D+AND+%22Mus+musculus%22%5Borganism%5D&retmax=100000&db=gene"
 
         geo_data_file_contents = Download(path, url, "geno-mouse").get_downloaded_file()
-
+        entrezIds = []
         data = json.loads(json.dumps(xmltodict.parse(geo_data_file_contents)))
         for k, v in data.items():
             print (k, v)
+            if k == 'IdList':
+                for entrezId in k:
+                   entrezIds.append(entrezId)
 
-        return "test"
+        return entrezIds
 
