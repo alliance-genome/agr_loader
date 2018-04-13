a68d91d93a7928c6bad60b39a81344f791795d29
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-04-13 08:21:23 -0700

    add some timing output

--- src/fetch_index.py
+++ src/fetch_index.py
@@ -1,5 +1,6 @@
 from aggregate_loader import AggregateLoader
 import os
+import time
 
 useTestObject = os.environ['TEST_SET']
 if useTestObject == "True":
@@ -12,8 +13,13 @@ uri = "bolt://" + host + ":" + port
 if __name__ == '__main__':
     al = AggregateLoader(uri, useTestObject)
 
+    start = time.time()
+    print("loader start time: " + start)
     # The following order is REQUIRED for proper loading.
     al.create_indicies()
     al.load_resource_descriptors()
     al.load_from_ontologies()
     al.load_from_mods()
+    end  = time.time()
+    print ("loader ent time: " + end)
+    print ("total time for loader: " + end - start)
