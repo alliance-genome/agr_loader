1e1048bcd8344b66498d48d8f7d00c76df6060d9
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-05-03 16:14:09 -0700

    stub out xref code

--- src/services/create_cross_reference.py
+++ src/services/create_cross_reference.py
@@ -1,31 +1,19 @@
 import uuid
-from loaders.transactions import Transaction
 
 
 class CreateCrossReference(object):
 
-    def get_xref(self, localId, prefix, xrefUrlMap, primaryId):
-
-        def __init__(self, localId, prefix, primaryId):
-            self.localId = localId
-            self.prefix = prefix
-            self.primaryId = primaryId
-
-
-
-
-
-# TODO: add bucket for panther
-crossRefPrimaryId = crossRef.get('id') + '_' + primary_id
-crossReferences.append({
-    "id": crossRefPrimaryId,
-    "globalCrossRefId": globalXrefId,
-    "localId": localCrossRefId,
-    "crossRefCompleteUrl": UrlService.get_no_page_complete_url(localCrossRefId, xrefUrlMap, prefix, primary_id),
-    "prefix": prefix,
-    "crossRefType": "gene/panther",
-    "primaryKey": crossRefPrimaryId + "gene/panther",
-    "uuid": str(uuid.uuid4()),
-    "page": "gene/panther",
-    "displayName": displayName
-})
+    def get_xref(self, localId, prefix, primaryId, crossRefType, page, displayName):
+        crossRefPrimaryId = prefix+":"+localId
+        crossReference = {
+            "id": crossRefPrimaryId,
+            "globalCrossRefId": crossRefPrimaryId,
+            "localId": localId,
+            "prefix": prefix,
+            "crossRefType": crossRefType,
+            "primaryKey": crossRefPrimaryId,
+            "uuid":  str(uuid.uuid4()),
+            "page": page,
+            "displayNae": displayName
+        }
+        return crossReference
