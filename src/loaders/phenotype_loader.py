53e2a410ad7524b4c9044cd44e1a0196145f0571
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-05-18 13:57:00 -0700

    stub out loader and transaction

---
+++ src/loaders/phenotype_loader.py
@@ -0,0 +1,11 @@
+from .transactions.phenotype import PhenotypeTransaction
+
+
+class PhenotypeLoader(object):
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def load_phenotype_objects(self, data):
+        tx = PhenotypeTransaction(self.graph)
+        tx.phenotype_object_tx(data)
