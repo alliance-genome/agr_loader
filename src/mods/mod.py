1084ca020345e809b54da5ecbe700619c0ce95d0
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2018-05-23 09:32:32 -0700

    fix path location based on Olin's PR

--- src/mods/mod.py
+++ src/mods/mod.py
@@ -86,7 +86,7 @@ class MOD(object):
 
     def load_phenotype_objects_mod(self, batch_size, testObject, phenotypeName, loadFile):
         path = "tmp"
-        S3File("mod-datadumps", loadFile, path).download()
+        S3File(loadFile, path).download()
         TARFile(path, loadFile).extract_all()
         phenotype_data = JSONFile().get_data(path + phenotypeName, 'phenotype')
         phenotype_dict = PhenotypeExt().get_phenotype_data(phenotype_data, batch_size, testObject)
