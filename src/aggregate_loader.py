94a12bdb321209294af39d348b9dd6648676b5f4
Sierra Moxon <staylor1@uoregon.edu>
2018-05-29 22:33:16 -0700

    more dataProvider changes

--- src/aggregate_loader.py
+++ src/aggregate_loader.py
@@ -46,14 +46,14 @@ class AggregateLoader(object):
         print("Downloading MI data.")
         self.mi_dataset = MIExt().get_data()
         # #
-        # print("Loading MI data into Neo4j.")
-        # MILoader(self.graph).load_mi(self.mi_dataset)
-        # print("Loading SO data into Neo4j.")
-        # SOLoader(self.graph).load_so(self.so_dataset)
-        # print("Loading GO data into Neo4j.")
-        # GOLoader(self.graph).load_go(self.go_dataset)
-        # print("Loading DO data into Neo4j.")
-        # DOLoader(self.graph).load_do(self.do_dataset)
+        print("Loading MI data into Neo4j.")
+        MILoader(self.graph).load_mi(self.mi_dataset)
+        print("Loading SO data into Neo4j.")
+        SOLoader(self.graph).load_so(self.so_dataset)
+        print("Loading GO data into Neo4j.")
+        GOLoader(self.graph).load_go(self.go_dataset)
+        print("Loading DO data into Neo4j.")
+        DOLoader(self.graph).load_do(self.do_dataset)
 
     def load_from_mods(self):
         print("Extracting BGI data from each MOD.")
