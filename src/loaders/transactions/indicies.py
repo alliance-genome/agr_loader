74445edc7f9b994651e425535ffbc16050e257b7
Sierra Moxon <staylor1@uoregon.edu>
2018-06-06 06:48:11 -0700

    add phenotype indicies

--- src/loaders/transactions/indicies.py
+++ src/loaders/transactions/indicies.py
@@ -44,7 +44,8 @@ class Indicies(object):
         session.run("CREATE INDEX ON :MITerm(primaryKey)")
         session.run("CREATE INDEX ON :Phenotype(primaryKey)")
         session.run("CREATE INDEX ON :PhenotypeEntityJoin(primaryKey)")
-        
+        session.run("CREATE INDEX ON :Entity(primaryKey)")
+
         #session.run("CREATE CONSTRAINT ON (g:Gene) ASSERT g.primaryKey IS UNIQUE;")
 
         #session.run("CREATE CONSTRAINT ON (n:Gene) ASSERT n.primaryKey IS UNIQUE")
