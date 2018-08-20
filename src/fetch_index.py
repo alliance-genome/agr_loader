32c3787e15cfe7be9b8250c86ef5662ec251a114
Sierra Moxon <staylor1@uoregon.edu>
2018-08-19 19:50:21 -0700

    fix index syntax

--- src/fetch_index.py
+++ src/fetch_index.py
@@ -13,17 +13,9 @@ uri = "bolt://" + host + ":" + port
 
 if __name__ == '__main__':
     al = AggregateLoader(uri, useTestObject)
-    # start = time.localtime(time.time())
-    # print("loader start time: ")
-    # print(start)
     # The following order is REQUIRED for proper loading.
     al.create_indices()
     al.load_from_ontologies()
     al.load_from_mods()
     gc.collect()
     al.load_additional_datasets()
-    # end = time.localtime(time.time())
-    # print ("loader ent time: ")
-    # print (end)
-    # print ("total time for loader: ")
-    # print (end - start)
