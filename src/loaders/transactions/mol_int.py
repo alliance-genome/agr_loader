50bd7e53d567e6d411a8c2dd75ce65d16d8b70b5
Chris Tabone <christabone@gmail.com>
2018-10-05 13:49:17 +0000

    Updated transactions for mol_into with CREATE functions.

--- src/loaders/transactions/mol_int.py
+++ src/loaders/transactions/mol_int.py
@@ -26,38 +26,38 @@ class MolIntTransaction(Transaction):
             MATCH (it:MITerm) WHERE it.primaryKey = row.interaction_type
 
             //Create the relationship between the two genes.
-            MERGE (g1)-[iw:INTERACTS_WITH {uuid:row.uuid}]->(g2)
+            CREATE (g1)-[iw:INTERACTS_WITH {uuid:row.uuid}]->(g2)
 
             //Create the Association node to be used for the object.
-            MERGE (oa:Association {primaryKey:row.uuid})
+            CREATE (oa:Association {primaryKey:row.uuid})
                 SET oa :InteractionGeneJoin
                 SET oa.joinType = 'molecular_interaction'
-            MERGE (g1)-[a1:ASSOCIATION]->(oa)
-            MERGE (oa)-[a2:ASSOCIATION]->(g2)
+            CREATE (g1)-[a1:ASSOCIATION]->(oa)
+            CREATE (oa)-[a2:ASSOCIATION]->(g2)
 
             //Create the publication nodes and link them to the Association node.
             MERGE (pn:Publication {primaryKey:row.pub_med_id})
                 SET pn.pubMedUrl = row.pub_med_url
                 SET pn.pubMedId = row.pub_med_id
-            MERGE (oa)-[ev:EVIDENCE]->(pn)
+            CREATE (oa)-[ev:EVIDENCE]->(pn)
 
             //Link detection method to the MI ontology.
-            MERGE (oa)-[dm:DETECTION_METHOD]->(mi)
+            CREATE (oa)-[dm:DETECTION_METHOD]->(mi)
 
             //Link source database to the MI ontology.
-            MERGE (oa)-[sd:SOURCE_DATABASE]->(sdb)
+            CREATE (oa)-[sd:SOURCE_DATABASE]->(sdb)
 
             //Link aggregation database to the MI ontology.
-            MERGE (oa)-[ad:AGGREGATION_DATABASE]->(adb)
+            CREATE (oa)-[ad:AGGREGATION_DATABASE]->(adb)
 
             //Link interactor roles and types to the MI ontology.
-            MERGE (oa)-[ita1:INTERACTOR_A_TYPE]->(ita)
-            MERGE (oa)-[itb1:INTERACTOR_B_TYPE]->(itb)
-            MERGE (oa)-[ira1:INTERACTOR_A_ROLE]->(ira)
-            MERGE (oa)-[irb1:INTERACTOR_B_ROLE]->(irb)
+            CREATE (oa)-[ita1:INTERACTOR_A_TYPE]->(ita)
+            CREATE (oa)-[itb1:INTERACTOR_B_TYPE]->(itb)
+            CREATE (oa)-[ira1:INTERACTOR_A_ROLE]->(ira)
+            CREATE (oa)-[irb1:INTERACTOR_B_ROLE]->(irb)
 
             //Link interaction type to the MI ontology.
-            MERGE (oa)-[it1:INTERACTION_TYPE]->(it)
+            CREATE (oa)-[it1:INTERACTION_TYPE]->(it)
 
             WITH oa, row.interactor_id_and_linkout AS events
                 UNWIND events AS event
@@ -73,7 +73,7 @@ class MolIntTransaction(Transaction):
                          id.primaryKey = event.primaryKey,
                          id.displayName = event.displayName
 
-                    MERGE (oa)-[gcr:CROSS_REFERENCE]->(id) """
+                    CREATE (oa)-[gcr:CROSS_REFERENCE]->(id) """
 
         Transaction.execute_transaction_batch(self, query, data, self.batch_size)
 
