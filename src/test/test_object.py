341bede88c1a60f1d77c402d4880c40bc51fe9ff
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2018-10-31 12:44:33 -0700

    add the gene so the test feature will be added

--- src/test/test_object.py
+++ src/test/test_object.py
@@ -75,7 +75,7 @@ class TestObject(object):
             'ZFIN:ZDB-GENE-990415-270', 'ZFIN:ZDB-LINCRNAG-160518-1', 'ZFIN:ZDB-GENE-030131-3776',
             'ZFIN:ZDB-GENE-030616-47',
             'ZFIN:ZDB-GENE-040426-1716', 'ZFIN:ZDB-ALT-980203-985', 'ZFIN:ZDB-ALT-060608-195',
-            'ZFIN:ZDB-ALT-050428-6', 'ZFIN:ZDB-ALT-151012-9',
+            'ZFIN:ZDB-ALT-050428-6', 'ZFIN:ZDB-ALT-151012-9', 'ZFIN:ZDB-GENE-070117-2142',
             # allele
             'ZFIN:ZDB-ALT-980413-591',
             # disease specific test objects
