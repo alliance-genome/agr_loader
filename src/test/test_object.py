f590b63e33c3ef028177df252ca794c8ad4221b6
Sierra Moxon <staylor1@uoregon.edu>
2018-10-30 20:42:38 -0700

    reduce test set agani

--- src/test/test_object.py
+++ src/test/test_object.py
@@ -18,14 +18,7 @@ class TestObject(object):
         self.mgiIdSet = {
             'MGI:5437116', 'MGI:1915135', 'MGI:109337', 'MGI:108202', 'MGI:2676586', 'MGI:88180', 'MGI:88467',
             'MGI:109583', 'MGI:96765', 'MGI:1099804',
-            'MGI:1916172', 'MGI:96680', 'MGI:2175810', 'MGI:5437110', 'MGI:5437073', 'MGI:88525',
-            'MGI:1914047', 'MGI:88053', 'MGI:88054', 'MGI:1855948',
-            'MGI:88056', 'MGI:88456', 'MGI:88447', 'MGI:88587', 'MGI:1338803', 'MGI:94864', 'MGI:1915101',
-            'MGI:1915112', 'MGI:1355324', 'MGI:3029164', 'MGI:1856330'
-                                                         'MGI:1915181', 'MGI:1915162', 'MGI:1915164', 'MGI:1929699',
             'MGI:94909', 'MGI:1856331', 'MGI:97490', 'MGI:108092', 'MGI:2156738',
-            'MGI:2148260', 'MGI:1856328', 'MGI:2678393', 'MGI:2429942', 'MGI:1856332', 'MGI:5569634',
-            'MGI:3531484', 'MGI:3531484',
             'MGI:2148259', 'MGI:3531483', 'MGI:1856329', 'MGI:3531484', 'MGI:5781149', 'MGI:2148259', 'MGI:104735',
             'MGI:98834',
             # phenotype objects
@@ -37,8 +30,6 @@ class TestObject(object):
         }
 
         self.wormbaseIdSet = {
-            'WB:WBGene00044305', 'WB:WBGene00169423', 'WB:WBGene00000987', 'WB:WBGene00021789',
-            'WB:WBGene00006750', 'WB:WBGene00000540', 'WB:WBGene00017866', 'WB:WBGene00001131',
             'WB:WBGene00015146', 'WB:WBGene00015599', 'WB:WBGene00001133', 'WB:WBGene00001115',
             'WB:WBGene00018468', 'WB:WBGene00019001', 'WB:WBGene00007438', 'WB:WBGene00001136', 'WB:WBGene00006742',
             'WB:WBGene00003883',
@@ -62,11 +53,9 @@ class TestObject(object):
 
         self.zfinIdSet = {
             'ZFIN:ZDB-GENE-990415-72', 'ZFIN:ZDB-GENE-030131-3445', 'ZFIN:ZDB-GENE-980526-388',
-            'ZFIN:ZDB-GENE-010525-1', 'ZFIN:ZDB-GENE-010525-1', 'ZFIN:ZDB-GENE-060117-5',
-            'ZFIN:ZDB-GENE-990415-270', 'ZFIN:ZDB-LINCRNAG-160518-1', 'ZFIN:ZDB-GENE-030131-3776',
-            'ZFIN:ZDB-GENE-030616-47',
+            'ZFIN:ZDB-GENE-990415-270', 'ZFIN:ZDB-LINCRNAG-160518-1',
             'ZFIN:ZDB-GENE-040426-1716', 'ZFIN:ZDB-ALT-980203-985', 'ZFIN:ZDB-ALT-060608-195',
-            'ZFIN:ZDB-ALT-050428-6', 'ZFIN:ZDB-ALT-151012-9',
+            'ZFIN:ZDB-ALT-050428-6', 'ZFIN:ZDB-ALT-151012-9', 'ZFIN:ZDB-GENE-030131-4430',
             # disease specific test objects
             'ZFIN:ZDB-GENE-030131-5607', 'ZFIN:ZDB-GENE-050517-20', 'ZFIN:ZDB-GENE-990415-122',
             'ZFIN:ZDB-GENE-980526-41',
@@ -83,10 +72,6 @@ class TestObject(object):
         }
         self.flybaseIdSet = {
             'FB:FBgn0083973', 'FB:FBgn0037960', 'FB:FBgn0027296', 'FB:FBgn0032006', 'FB:FBgn0001319',
-            'FB:FBgn0002369', 'FB:FBgn0026379',
-            'FB:FBgn0033885', 'FB:FBgn0024320', 'FB:FBgn0283499',
-            'FB:FBgn0032465', 'FB:FBgn0285944', 'FB:FBgn0032728', 'FB:FBgn0000014',
-            'FB:FBgn0032729', 'FB:FBgn0065610', 'FB:FBgn0032730', 'FB:FBgn0032732', 'FB:FBgn0260987',
             'FB:FBgn0032781', 'FB:FBgn0032782', 'FB:FBgn0032740',
             'FB:FBgn0032741', 'FB:FBgn0032744', 'FB:FBgn0036309', 'FB:FBgn0003470', 'FB:FBal0161187', 'FB:FBal0000003',
             'FB:FBal0000004', 'FB:FBgn0039156',
@@ -98,7 +83,6 @@ class TestObject(object):
         }
         self.rgdTestSet = {
             'RGD:70891', 'RGD:1306349', 'RGD:708528', 'RGD:620796', 'RGD:61995', 'RGD:1309165',
-            'RGD:1581495', 'RGD:2322065', 'RGD:1309063', 'RGD:2845', 'RGD:628748', 'RGD:1581476',
             'RGD:1309312', 'RGD:7627512', 'RGD:1309105', 'RGD:1309109', 'RGD:7627503', 'RGD:1578801',
             # disease pheno specific test objects
             'RGD:68936', 'RGD:3886', 'RGD:3673', 'RGD:6498788', 'RGD:1303329',
