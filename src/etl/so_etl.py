cae3c5c0da65dc9e45f52bcfd8e0b44d6cc5cc9d
Olin Blodgett <Olin.Blodgett@jax.org>
2018-11-01 16:16:38 -0400

    Made query static

--- src/etl/so_etl.py
+++ src/etl/so_etl.py
@@ -8,17 +8,18 @@ logger = logging.getLogger(__name__)
 
 class SOETL(ETL):
 
+    query_template = """
+        USING PERIODIC COMMIT 10000
+        LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
+
+        MERGE (s:SOTerm:Ontology {primaryKey:row.id})
+            SET s.name = row.name """
+
     def __init__(self, data_manager):
         self.data_type_config = data_manager.get_config("SO")
-        self.query_template = """
-            USING PERIODIC COMMIT 10000
-            LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
-
-            MERGE (s:SOTerm:Ontology {primaryKey:row.id})
-                SET s.name = row.name """
 
     def _running_etl(self):
-        return self.data_type_config.running_etl()
+        return self.data_type_config != None and self.data_type_config.running_etl()
 
     def _load_and_process_data(self):
         #for mod in mods
@@ -28,7 +29,7 @@ class SOETL(ETL):
 
         data = self.data_type_config.get_data()
         generator = self.get_generators(data)
-        Neo4jTransactor.execute_transaction(generator, "so_data.csv", self.query_template)
+        Neo4jTransactor.execute_transaction(generator, "so_data.csv", SOETL.query_template)
 
     def get_generators(self, data):
         so_list = []
