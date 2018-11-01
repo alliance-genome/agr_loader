2e3ac520cf2ca4e5edf6aec8194c23f9ff7287d2
Chris Tabone <christabone@gmail.com>
2018-11-01 16:31:04 -0300

    Update ortho_ext.py
    Fixed typo.

--- src/extractors/ortho_ext.py
+++ src/extractors/ortho_ext.py
@@ -17,10 +17,10 @@ class OrthoExt(object):
     def get_data(testObject, mod_name, batch_size):
         path = "tmp"
         if testObject.using_test_data() is True:
-            filename = 'orthology_test_data_1.0.0.7_2.json'
+            filename = 'orthology_test_data_1.0.0.7_3.json'
             filename_comp = 'ORTHO/orthology_test_data_1.0.0.7_3.json.tar.gz'
         else:
-            filename = "orthology_" + mod_name + "_1.0.0.7_2.json"
+            filename = "orthology_" + mod_name + "_1.0.0.7_3.json"
             filename_comp = "ORTHO/orthology_" + mod_name + "_1.0.0.7_3.json.tar.gz"
 
         S3File(filename_comp, path).download()
