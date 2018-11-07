a194a5aebc6fb1a030789ba15807c94dccd8f5b4
Christopher Tabone <christabone@gmail.com>
2018-11-07 14:48:27 +0000

    First updates to data file manager.

--- src/data_manager/data_file_manager.py
+++ src/data_manager/data_file_manager.py
@@ -1,5 +1,7 @@
 from files import *
-import logging, yaml
+import logging, yaml, os, pprint, sys
+from .data_type_config import DataTypeConfig
+from .mod_config import ModConfig
 from cerberus import Validator
 from services import taxon_to_MOD
 
@@ -7,27 +9,36 @@ logger = logging.getLogger(__name__)
 
 class DataFileManager(object):
     
-    def __init__(self, configfile):
-        logger.info('Loading config file: %s' % (configfile))
-        self.config_data = open(configfile, 'r')
+    def __init__(self, config_file_loc):
+        logger.info('Loading config file: %s' % (config_file_loc))
+        config_file = open(config_file_loc, 'r')
+        self.config_data = yaml.load(config_file)
 
-        validation_yaml = 'config/validation.yml'
-        logger.info('Loading validation schema: %s' % (validation_yaml))
-        self.validation_schema = open(validation_yaml, 'r')
+        validation_yaml_file_loc = os.path.abspath('src/config/validation.yml')
+        logger.info('Loading validation schema: %s' % (validation_yaml_file_loc))
+        validation_schema_file = open(validation_yaml_file_loc, 'r')
+        self.validation_schema = yaml.load(validation_schema_file)
 
         # Loading a JSON blurb from a file as a placeholder for submission system query.
-        self.mock_submission_system = 'config/mock_submission_system.json'
-        logger.info('Loading mock submission system JSON: %s' % (self.mock_submission_system))
+        mock_submission_system_file_loc = os.path.abspath('src/config/mock_submission_system.json')
+        self.submission_system_data = JSONFile().get_data(mock_submission_system_file_loc)
 
-        # Dictionary used for storing config file information.
-        self.config_dictionary = {}
+        # List used for MOD and data type objects.
+        self.master_data_dictionary = {}
 
-    def taxon_to_mod(self, taxon):
+    def get_config(self, type_of_data):
         pass
 
-    def download_and_validate(self):
-        pass
+    def dispatch_to_object(self):
+        dispatch_dictionary = {
+            'BGI': ModConfig,
+        }
 
+        for config_entry in self.config_data.keys():
+            logger.info('Processing data type: %s' % (config_entry))
+            print(config_entry)
+            print(self.config_data[config_entry])
+        
     def process_config(self):
         validator = Validator(self.validation_schema)
         validation_results = validator.validate(self.config_data)
@@ -35,15 +46,24 @@ class DataFileManager(object):
         if validation_results is True:
             logger.info('Config file validation successful.')
         else:
-            logger.critical('Config file validation successful.')
+            logger.critical('Config file validation unsuccessful!')
             for field, values in validator.errors.items():
                 for value in values: # May have more than one error per field.
                     message = field + ': ' + value
                     logger.critical(message)
+            logger.critical('Exiting')
+            sys.exit(-1)
+        
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(self.config_data)
+        pp.pprint(self.submission_system_data)
 
-        print(self.config_data)
-        print(self.mock_submission_system)
-        quit()
+        # Create our config objects, either "DataType" or "MOD".
+        self.dispatch_to_object()
+
+        # for data_object in self.submission_system_data['dataFiles']:
+        #     print(data_object['dataType'])
+        # quit()
 
         # More logic here to generate config object
         pass
