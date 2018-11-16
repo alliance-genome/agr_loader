7c5d35560401290c0dda5d17ea150a2bb8830c4d
Christopher Tabone <christabone@gmail.com>
2018-11-16 17:47:12 +0000

    Troubleshooting log output and remove GFF extraction.

--- src/files/tar_file.py
+++ src/files/tar_file.py
@@ -13,13 +13,19 @@ class TARFile(object):
     def extract_all(self):
         logger.info("Reading header from %s/%s ..." % (self.path, self.tarfilename))
 
-        tfile = tarfile.open(self.path + "/" + self.tarfilename, 'r')
+        members_to_extract = []
         extract = False
+        tfile = tarfile.open(self.path + "/" + self.tarfilename, 'r')
         for member in tfile.getmembers():
+            lower_name = member.name.lower()
+            if 'gff' in lower_name:
+                logger.info('Skipping GFF file extraction for %s' % (member.name))
+                continue
             if not os.path.exists(self.path + "/" + member.name):
                 logger.info("Extracting (%s->%s/%s)" % (member.name, self.path, member.name))
+                members_to_extract.append(member)
                 extract = True
             else:
                 logger.info('%s/%s already exists, not extracting.' % (self.path, member.name))
         if extract is True:
-            tfile.extractall(self.path)
+            tfile.extractall(self.path, members_to_extract)
