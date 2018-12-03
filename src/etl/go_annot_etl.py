7e16a8299e02c307fb0210d5eef8fc69957f0cae
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2018-12-03 15:10:18 -0800

    simplify goannot query and processing

--- src/etl/go_annot_etl.py
+++ src/etl/go_annot_etl.py
@@ -1,22 +1,20 @@
 import logging, uuid, gzip, csv
+
 logger = logging.getLogger(__name__)
 
 from transactors import CSVTransactor
 
 from etl import ETL
 from etl.helpers import ETLHelper
-from files import JSONFile, S3File
+
 
 class GOAnnotETL(ETL):
-
     query_template = """
         USING PERIODIC COMMIT %s
-        LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
-            
+            LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
             MATCH (g:Gene {primaryKey:row.gene_id})
-            FOREACH (entry in row.annotations |
-                MERGE (go:GOTerm:Ontology {primaryKey:entry.go_id})
-                MERGE (g)-[:ANNOTATED_TO]->(go)) """
+            MATCH (go:GOTerm:Ontology {primaryKey:row.go_id})
+            MERGE (g)-[ggo:ANNOTATED_TO]->(go) """
 
     def __init__(self, config):
         super().__init__()
@@ -28,60 +26,48 @@ class GOAnnotETL(ETL):
             logger.info("Loading GOAnnot Data: %s" % sub_type.get_data_provider())
             filepath = sub_type.get_file_to_download()
             file = gzip.open("tmp/" + filepath, 'rt', encoding='utf-8')
-            
+
             logger.info("Finished Loading GOAnnot Data: %s" % sub_type.get_data_provider())
 
-            # This order is the same as the lists yielded from the get_generators function.    
+            # This order is the same as the lists yielded from the get_generators function.
             # A list of tuples.
 
             commit_size = self.data_type_config.get_neo4j_commit_size()
-            #batch_size = self.data_type_config.get_generator_batch_size()
+            batch_size = self.data_type_config.get_generator_batch_size()
 
             generators = self.get_generators(
                 file,
                 ETLHelper.go_annot_prefix_lookup(sub_type.get_data_provider()),
+                batch_size
             )
 
             query_list = [
                 [GOAnnotETL.query_template, commit_size, "go_annot_" + sub_type.get_data_provider() + ".csv"],
             ]
-            
+
             CSVTransactor.execute_transaction(generators, query_list)
 
-
-    def get_generators(self, file, prefix):
-        
-
-        go_annot_dict = {}
+    def get_generators(self, file, prefix, batch_size):
         go_annot_list = []
-
+        counter = 0
         reader = csv.reader(file, delimiter='\t')
         for line in reader:
             if line[0].startswith('!'):
                 continue
-            gene_id = prefix + line[1]
+            gene = prefix + line[1]
+            logger.info("gene is: " + gene)
             go_id = line[4]
+            go_annot_dict = {
+                'gene_id': gene,
+                'go_id': go_id
+            }
+            counter = counter + 1
+            go_annot_list.append(go_annot_dict)
+            if counter == batch_size:
+                counter = 0
+                yield [go_annot_list]
+                go_annot_list = []
 
-            if gene_id in go_annot_dict:
-                go_annot_dict[gene_id]['annotations'].append({"go_id": go_id})
-            else:
-                go_annot_dict[gene_id] = {
-                    'gene_id': gene_id,
-                    'annotations': [{"go_id": go_id}],
-                }
-        # Convert the dictionary into a list of dictionaries for Neo4j.
-        # Check for the use of testObject and only return test data if necessary.
-        if self.testObject.using_test_data() is True:
-            for entry in go_annot_dict:
-                if self.testObject.check_for_test_id_entry(go_annot_dict[entry]['gene_id']) is True:
-                    go_annot_list.append(go_annot_dict[entry])
-                    self.testObject.add_ontology_ids([annotation["go_id"] for annotation in go_annot_dict[entry]['annotations']])
-                else:
-                    continue
+        if counter > 0:
             yield [go_annot_list]
-            file.close()
-        else:
-            for entry in go_annot_dict:
-                go_annot_list.append(go_annot_dict[entry])
-            yield [go_annot_list]
-            file.close()
+        file.close()
