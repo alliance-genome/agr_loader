23dfd55a5b0d897621c9d4ee30aabb03ee6e6062
Christopher Tabone <christabone@gmail.com>
2019-02-12 13:34:12 +0000

    Consolidated xref code.

--- src/etl/molecular_interaction_etl.py
+++ src/etl/molecular_interaction_etl.py
@@ -4,101 +4,89 @@ logger = logging.getLogger(__name__)
 import uuid, csv, re, sys, itertools
 
 from .helpers import ResourceDescriptorHelper2, Neo4jHelper
+from etl.helpers import ETLHelper
 from etl import ETL
 from transactors import CSVTransactor, Neo4jTransactor
 
 class MolecularInteractionETL(ETL):
 
     query_template = """
-            USING PERIODIC COMMIT %s
-            LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
+        USING PERIODIC COMMIT %s
+        LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
 
-            MATCH (g1:Gene {primaryKey:row.interactor_A})
-            MATCH (g2:Gene {primaryKey:row.interactor_B})
+        MATCH (g1:Gene {primaryKey:row.interactor_A})
+        MATCH (g2:Gene {primaryKey:row.interactor_B})
 
-            MATCH (mi:MITerm) WHERE mi.primaryKey = row.detection_method
-            MATCH (sdb:MITerm) WHERE sdb.primaryKey = row.source_database
-            MATCH (adb:MITerm) WHERE adb.primaryKey = row.aggregation_database
-            MATCH (ita:MITerm) WHERE ita.primaryKey = row.interactor_A_type
-            MATCH (itb:MITerm) WHERE itb.primaryKey = row.interactor_B_type
-            MATCH (ira:MITerm) WHERE ira.primaryKey = row.interactor_A_role
-            MATCH (irb:MITerm) WHERE irb.primaryKey = row.interactor_B_role
-            MATCH (it:MITerm) WHERE it.primaryKey = row.interaction_type
+        MATCH (mi:MITerm) WHERE mi.primaryKey = row.detection_method
+        MATCH (sdb:MITerm) WHERE sdb.primaryKey = row.source_database
+        MATCH (adb:MITerm) WHERE adb.primaryKey = row.aggregation_database
+        MATCH (ita:MITerm) WHERE ita.primaryKey = row.interactor_A_type
+        MATCH (itb:MITerm) WHERE itb.primaryKey = row.interactor_B_type
+        MATCH (ira:MITerm) WHERE ira.primaryKey = row.interactor_A_role
+        MATCH (irb:MITerm) WHERE irb.primaryKey = row.interactor_B_role
+        MATCH (it:MITerm) WHERE it.primaryKey = row.interaction_type
 
-            //Create the relationship between the two genes.
-            CREATE (g1)-[iw:INTERACTS_WITH {uuid:row.uuid}]->(g2)
+        //Create the relationship between the two genes.
+        CREATE (g1)-[iw:INTERACTS_WITH {uuid:row.uuid}]->(g2)
 
-            //Create the Association node to be used for the object.
-            CREATE (oa:Association {primaryKey:row.uuid})
-                SET oa :InteractionGeneJoin
-                SET oa.joinType = 'molecular_interaction'
-            CREATE (g1)-[a1:ASSOCIATION]->(oa)
-            CREATE (oa)-[a2:ASSOCIATION]->(g2)
+        //Create the Association node to be used for the object.
+        CREATE (oa:Association {primaryKey:row.uuid})
+            SET oa :InteractionGeneJoin
+            SET oa.joinType = 'molecular_interaction'
+        CREATE (g1)-[a1:ASSOCIATION]->(oa)
+        CREATE (oa)-[a2:ASSOCIATION]->(g2)
 
-            //Create the publication nodes and link them to the Association node.
-            MERGE (pn:Publication {primaryKey:row.pub_med_id})
-                ON CREATE SET pn.pubMedUrl = row.pub_med_url,
-                pn.pubMedId = row.pub_med_id
-            CREATE (oa)-[ev:EVIDENCE]->(pn)
+        //Create the publication nodes and link them to the Association node.
+        MERGE (pn:Publication {primaryKey:row.pub_med_id})
+            ON CREATE SET pn.pubMedUrl = row.pub_med_url,
+            pn.pubMedId = row.pub_med_id
+        CREATE (oa)-[ev:EVIDENCE]->(pn)
 
-            //Link detection method to the MI ontology.
-            CREATE (oa)-[dm:DETECTION_METHOD]->(mi)
+        //Link detection method to the MI ontology.
+        CREATE (oa)-[dm:DETECTION_METHOD]->(mi)
 
-            //Link source database to the MI ontology.
-            CREATE (oa)-[sd:SOURCE_DATABASE]->(sdb)
+        //Link source database to the MI ontology.
+        CREATE (oa)-[sd:SOURCE_DATABASE]->(sdb)
 
-            //Link aggregation database to the MI ontology.
-            CREATE (oa)-[ad:AGGREGATION_DATABASE]->(adb)
+        //Link aggregation database to the MI ontology.
+        CREATE (oa)-[ad:AGGREGATION_DATABASE]->(adb)
 
-            //Link interactor roles and types to the MI ontology.
-            CREATE (oa)-[ita1:INTERACTOR_A_TYPE]->(ita)
-            CREATE (oa)-[itb1:INTERACTOR_B_TYPE]->(itb)
-            CREATE (oa)-[ira1:INTERACTOR_A_ROLE]->(ira)
-            CREATE (oa)-[irb1:INTERACTOR_B_ROLE]->(irb)
+        //Link interactor roles and types to the MI ontology.
+        CREATE (oa)-[ita1:INTERACTOR_A_TYPE]->(ita)
+        CREATE (oa)-[itb1:INTERACTOR_B_TYPE]->(itb)
+        CREATE (oa)-[ira1:INTERACTOR_A_ROLE]->(ira)
+        CREATE (oa)-[irb1:INTERACTOR_B_ROLE]->(irb)
 
-            //Link interaction type to the MI ontology.
-            CREATE (oa)-[it1:INTERACTION_TYPE]->(it)
+        //Link interaction type to the MI ontology.
+        CREATE (oa)-[it1:INTERACTION_TYPE]->(it)
     """
 
     query_xref = """
-            USING PERIODIC COMMIT %s
-            LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
+        USING PERIODIC COMMIT %s
+        LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
 
-            // This needs to be a MERGE below.
-            MATCH (g1:Gene) WHERE g1.primaryKey = row.primaryKey
-                MERGE (id:CrossReference:Identifier {primaryKey:row.primaryKey})
-                    ON CREATE SET id.name = row.name,
-                        id.globalCrossRefId = row.globalCrossRefId,
-                        id.localId = row.localId,
-                        id.crossRefCompleteUrl = row.crossRefCompleteUrl,
-                        id.prefix = row.prefix,
-                        id.crossRefType = row.crossRefType,
-                        id.uuid = row.uuid,
-                        id.page = row.page,
-                        id.primaryKey = row.primaryKey,
-                        id.displayName = row.displayName
+        // This needs to be a MERGE below.
+        MATCH (g1:Gene) WHERE g1.primaryKey = row.primaryKey
+            MERGE (id:CrossReference:Identifier {primaryKey:row.primaryKey})
+                ON CREATE SET id.name = row.name,
+                    id.globalCrossRefId = row.globalCrossRefId,
+                    id.localId = row.localId,
+                    id.crossRefCompleteUrl = row.crossRefCompleteUrl,
+                    id.prefix = row.prefix,
+                    id.crossRefType = row.crossRefType,
+                    id.uuid = row.uuid,
+                    id.page = row.page,
+                    id.primaryKey = row.primaryKey,
+                    id.displayName = row.displayName
 
-                MERGE (oa)-[gcr:CROSS_REFERENCE]->(id) """
+            MERGE (oa)-[gcr:CROSS_REFERENCE]->(id) """
 
     query_mod_xref = """
-            USING PERIODIC COMMIT %s
-            LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
 
-            // This needs to be a MERGE below.
-            MATCH (o:Gene {primaryKey:row.dataId})
-                MERGE (id:CrossReference:Identifier {primaryKey:row.primaryKey})
-                    ON CREATE SET id.name = row.name,
-                        id.globalCrossRefId = row.globalCrossRefId,
-                        id.localId = row.localId,
-                        id.crossRefCompleteUrl = row.crossRefCompleteUrl,
-                        id.prefix = row.prefix,
-                        id.crossRefType = row.crossRefType,
-                        id.uuid = row.uuid,
-                        id.page = row.page,
-                        id.primaryKey = row.primaryKey,
-                        id.displayName = row.displayName
+        USING PERIODIC COMMIT %s
+        LOAD CSV WITH HEADERS FROM \'file:///%s\' AS row
 
-                MERGE (o)-[gcr:CROSS_REFERENCE]->(id) """             
+            MATCH (o:Gene {primaryKey:row.dataId}) """ + ETLHelper.get_cypher_xref_text()        
 
     def __init__(self, config):
         super().__init__()
