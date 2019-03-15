ba47847c804ee9fd7b44d779296f82cbe376ced2
Olin Blodgett <nilo@nilosplace.net>
2019-03-15 16:40:38 -0400

    Fix for import ordering

--- src/aggregate_loader.py
+++ src/aggregate_loader.py
@@ -1,10 +1,10 @@
 import logging, coloredlogs, os, multiprocessing, time, argparse, time
 
-from common import ContextInfo
-from data_manager import DataFileManager
 from etl import *
 from etl.helpers import Neo4jHelper
 from transactors import Neo4jTransactor, FileTransactor
+from data_manager import DataFileManager
+from common import ContextInfo # Must be the last timeport othersize program fails
 
 
 parser = argparse.ArgumentParser(description='Load data into the Neo4j database for the Alliance of Genome Resources.')
