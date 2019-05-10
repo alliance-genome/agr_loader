703a7b7d7049df2bffb4dcdf64e828fe266182f6
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2019-05-10 14:35:47 -0700

    add indexes

--- src/test/schema_node_tests.py
+++ src/test/schema_node_tests.py
@@ -84,6 +84,7 @@ class TestClass(object):
                             dict(node='GOTerm', prop='primaryKey'),
                             dict(node='Gene', prop='dataProvider'),
                             dict(node='AffectedGenomicModel', prop='primaryKey'),
+                            dict(node='AffectedGenomicModel', prop='name'),
                             #dict(node='SOTerm', prop='name'),
                             dict(node='SOTerm', prop='primaryKey'),
                             dict(node='DOTerm', prop='doPrefix'),
@@ -143,6 +144,7 @@ class TestClass(object):
                             ],
 
         'test_prop_not_null': [dict(node='AffectedGenomicModel', prop='primaryKey'),
+                               dict(node='AffectedGenomicModel', prop='name'),
                                dict(node='Gene', prop='modGlobalCrossRefId'),
                                dict(node='Variant', prop='primaryKey'),
                                dict(node='Gene', prop='geneLiteratureUrl'),
@@ -234,6 +236,7 @@ class TestClass(object):
                              dict(node='Stage', prop='primaryKey'),
                              dict(node='SequenceTargetingReagent', prop='primaryKey'),
                              dict(node='AffectedGenomicModel', prop='primaryKey'),
+                             dict(node='AffectedGenomicModel', prop='name'),
                              # with uberon, this can not be unique any longer, unless
                              # every term is just 'ontology' not ontology-specific node labels.
                              # dict(node='Ontology', prop='primaryKey'),
