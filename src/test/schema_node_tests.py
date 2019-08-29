aedb3dbe5a0ba707d8a95c699e3ebe72336c6d7e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2019-08-29 16:15:37 -0700

    remove json parser for ontologies for now

--- src/test/schema_node_tests.py
+++ src/test/schema_node_tests.py
@@ -138,8 +138,7 @@ class TestClass(object):
                             dict(node='ExpressionBioEntity', prop='whereExpressedStatement'),
                             dict(node='BioEntityGeneExpressionJoin', prop='primaryKey'),
                             dict(node='DOTerm', prop='defLinks'),
-                            dict(node='Variant', prop='primaryKey'),
-                            dict(node='Variant', prop='geneLevelConsequence')
+                            dict(node='Variant', prop='primaryKey')
 
                             ],
 
