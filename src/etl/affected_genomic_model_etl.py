53e47890fc7aa1bf7fcfa5e60f721449f7b84ec6
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2019-09-30 13:46:57 -0700

    add fish clause

--- src/etl/affected_genomic_model_etl.py
+++ src/etl/affected_genomic_model_etl.py
@@ -223,7 +223,7 @@ class AffectedGenomicModelETL(ETL):
                 # some pages collection have 0 elements
                 if pages is not None and len(pages) > 0:
                     for page in pages:
-                        if page == 'fish' or page == 'genotype' or page == 'strain':
+                        if page == 'Fish' or page == 'genotype' or page == 'strain':
                             modGlobalCrossRefUrl = ETLHelper.get_page_complete_url(local_crossref_id,
                                                                                        self.xrefUrlMap, prefix, page)
 
