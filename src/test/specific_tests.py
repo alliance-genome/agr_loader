fe35aa04c8d1ba671c4f5505550cc528c38dc5b6
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2020-05-16 06:53:50 -0700

    change syntax of test for PR

--- src/test/specific_tests.py
+++ src/test/specific_tests.py
@@ -763,7 +763,7 @@ def test_mi_term_has_corrected_url():
 
 
 def test_rgd_dej_has_rgd_full_url_cross_reference():
-    query = """MATCH (g:Gene)--(dej:DiseaseEntityJoin)--(cr:CrossReference), (dej)--(do:DOTerm)
+    query = """MATCH (g:Gene)--(dej:DiseaseEntityJoin)--(cr:CrossReference)
             WHERE g.primaryKey = 'RGD:2004'
             and cr.globalCrossRefId = 'DOID:583'
             AND cr.crossRefCompleteUrl = 'https://rgd.mcw.edu/rgdweb/ontology/annot.html?species=Rat&x=1&acc_id=583#annot'
@@ -774,10 +774,9 @@ def test_rgd_dej_has_rgd_full_url_cross_reference():
 
 
 def test_human_dej_has_omim_full_url_cross_reference():
-    query = """MATCH (g:Gene)--(dej:DiseaseEntityJoin)--(cr:CrossReference), (dej)--(do:DOTerm)
+    query = """MATCH (g:Gene)--(dej:DiseaseEntityJoin)--(cr:CrossReference)
             WHERE g.primaryKey = 'HGNC:12597'
             and cr.globalCrossRefId = 'OMIM:605242'
-            and do.primaryKey = 'DOID:0110473'
             AND cr.crossRefCompleteUrl = 'https://www.omim.org/entry/605242'
             RETURN count(distinct(cr)) AS counter"""
     result = execute_transaction(query)
