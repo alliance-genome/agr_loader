b008c49d3a03625773e82485f4736f25fd77492e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2020-06-15 14:57:27 -0700

    add more tests

--- src/test/specific_tests.py
+++ src/test/specific_tests.py
@@ -1186,14 +1186,28 @@ def test_pseudogenic_transcript_exists():
         assert record["counter"] > 0
 
 
-def test_codon_protein_consequence_exists():
-    """Test FB variant has protein and codon start/end """
+def test_protein_consequence_exists():
+    """Test FB variant has protein start/end """
 
     query = """ MATCH (v:Variant)--(tlc:TranscriptLevelConsequence)
                     WHERE v.primaryKey = 'NT_033779.5:g.5464013C>T'
                     AND tlc.proteinRange IS NOT NULL
                     AND tlc.proteinStartPosition IS NOT NULL
                     AND tlc.proteinEndPosition IS NOT NULL
+                RETURN count(v) AS counter """
+    result = execute_transaction(query)
+    for record in result:
+        assert record["counter"] > 0
+
+
+def test_codon_consequence_exists():
+    """Test FB variant has codon start/end """
+
+    query = """ MATCH (v:Variant)--(tlc:TranscriptLevelConsequence)
+                    WHERE v.primaryKey = 'NT_033779.5:g.5464013C>T'
+                    AND tlc.codonChange IS NOT NULL
+                    AND tlc.codonReference IS NOT NULL
+                    AND tlc.codonVariation IS NOT NULL
                 RETURN count(v) AS counter """
     result = execute_transaction(query)
     for record in result:
