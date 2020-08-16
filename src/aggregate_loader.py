3c4adca430bea8b13072f74542b37b43d8c53712
Ian Longden <ilongden@Ians-MacBook-Pro.local>
2020-08-15 21:17:20 -0400

    more info

--- src/aggregate_loader.py
+++ src/aggregate_loader.py
@@ -150,12 +150,14 @@ class AggregateLoader():
             if os.path.exists(name):
                 self.logger.warning("*********WARNING: removing old {} file.".format(name))
                 os.remove(name)
+        self.logger.info("Getting files initially")
         url = 'https://raw.githubusercontent.com/alliance-genome/agr_schemas/SCHEMA_BRANCH/resourceDescriptors.yaml'
         url = url.replace('SCHEMA_BRANCH', self.schema_branch)
         Download('tmp', url, 'resourceDescriptors.yaml').get_downloaded_data()
         url = 'https://raw.githubusercontent.com/alliance-genome/agr_schemas/SCHEMA_BRANCH/ingest/species/species.yaml'
         url = url.replace('SCHEMA_BRANCH', self.schema_branch)
         Download('tmp', url, 'species.yaml').get_downloaded_data()
+        self.logger.info("Finished getting files initially")
 
     @classmethod
     def run_etl_groups(cls, logger, data_manager, neo_transactor):
