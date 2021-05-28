f9c72e7a1ad8bb33745aac5edbb812180ba16e6a
Manuel Luypaert <luypaert@ebi.ac.uk>
2021-05-28 22:17:27 +0100

    Updated ecUniqueKey to include condition statement (creating more unique ExperimentalCondition nodes), AGR-2925, AGR-2541

--- src/etl/helpers/experimental_condition_helper.py
+++ src/etl/helpers/experimental_condition_helper.py
@@ -60,7 +60,8 @@ class ExperimentalConditionHelper():
             for condition in relation['conditions']:
                 # Store unique conditions
                 # Unique condition key: conditionClassId + (anatomicalOntologyId | chemicalOntologyId | geneOntologyId | NCBITaxonID)
-                unique_key = condition.get('conditionClassId') \
+                unique_key = str( condition.get('conditionStatement') or '' ) \
+                              + condition.get('conditionClassId') \
                               + str( condition.get('conditionId') or '' ) \
                               + str( condition.get('anatomicalOntologyId') or '' ) \
                               + str( condition.get('chemicalOntologyId') or '' ) \
