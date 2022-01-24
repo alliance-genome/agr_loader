43ff6d22c1c4bf326e841fca6ac85e60c9b20db2
markquintontulloch <markquintontulloch@gmail.com>
2022-01-24 13:37:02 +0000

    Don't replace full stops in assembly names

--- src/etl/helpers/assembly_sequence_helper.py
+++ src/etl/helpers/assembly_sequence_helper.py
@@ -14,7 +14,7 @@ class AssemblySequenceHelper():
     logger = logging.getLogger(__name__)
 
     def __init__(self, assembly, data_manager):
-        sub_type = assembly.replace('.', '').replace('_', '')
+        sub_type = assembly.replace('_', '')
         if sub_type.startswith('R6'):
             sub_type = 'R627'
         fasta_config = data_manager.get_config('FASTA')
