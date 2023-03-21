a8de67fa467fb12913b0dfd7fe156e60c52d3f76
Christopher Tabone <christabone@gmail.com>
2023-03-21 16:49:09 -0300

    Updated Fish fix.

--- src/etl/affected_genomic_model_etl.py
+++ src/etl/affected_genomic_model_etl.py
@@ -180,7 +180,7 @@ class AffectedGenomicModelETL(ETL):
         # some pages collection have 0 elements
         if pages is not None and len(pages) > 0:
             for page in pages:
-                if page in ['Fish', 'genotype', 'strain']:
+                if page.lower() in ['fish', 'genotype', 'strain']:
                     cross_ref = self.etlh.rdh2.return_url_from_key_value(
                         prefix, local_crossref_id, alt_page=page)
         return cross_ref
