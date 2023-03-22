ea762299bae170adedb1022fdddcc70862ccad51
Christopher Tabone <christabone@gmail.com>
2023-03-22 11:53:39 -0300

    Adding a wait for queue before overlapping yields.

--- src/etl/phenotype_etl.py
+++ src/etl/phenotype_etl.py
@@ -344,6 +344,9 @@ class PhenoTypeETL(ETL):
                 counter = 0
 
         if counter > 0:
+            self.logger.info('Waiting for queues to clear before last load.')
+            Neo4jTransactor().wait_for_queues()
+
             yield [ list_to_yield,
                     list_to_yield,
                     list_to_yield,
