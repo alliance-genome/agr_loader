c0818714975a18e6734c223aeaa2a0982c03b034
Christopher Tabone <christabone@gmail.com>
2023-03-22 19:40:05 -0300

    Reverted transactor tests.

--- src/transactors/neo4j_transactor.py
+++ src/transactors/neo4j_transactor.py
@@ -72,7 +72,6 @@ class Neo4jTransactor():
         if context_info.env["USING_PICKLE"] is False:
             uri = "bolt://" + context_info.env["NEO4J_HOST"] + ":" + str(context_info.env["NEO4J_PORT"])
             graph = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"), max_connection_pool_size=-1, fetch_size=10000)
-            session = graph.session()
             watch("neo4j")
 
         self.logger.info("%s: Starting Neo4jTransactor Thread Runner: ", self._get_name())
@@ -99,13 +98,11 @@ class Neo4jTransactor():
                         # Save VIA pickle rather then NEO
                         file_name = "tmp/temp/transaction_%s_%s" % (query_counter, total_query_counter)
                         with open(file_name, 'wb') as file:
-                            self.logger.debug("Writting to file: tmp/temp/transaction_%s_%s", query_counter, total_query_counter)
+                            self.logger.debug("Writing to file: tmp/temp/transaction_%s_%s", query_counter, total_query_counter)
                             pickle.dump(neo4j_query, file)
                     else:
-                        with session.begin_transaction() as tx:
-                            tx.run(neo4j_query)
-                            tx.commit()
-                            tx.close()
+                        with graph.session() as session:
+                            session.run(neo4j_query)
 
                     end = time.time()
                     elapsed_time = end - start
