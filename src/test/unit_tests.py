a303176d6d0d5bfc03e02d0f3d5c3652125c40ce
Chris Tabone <christabone@gmail.com>
2024-06-24 21:21:27 -0300

    Update unit_tests.py
    Unit test fix again.

--- src/test/unit_tests.py
+++ src/test/unit_tests.py
@@ -68,26 +68,26 @@ class TestClass():
         self.etlh.rdh2.missing_pages = {}
         self.etlh.rdh2.bad_pages = {}
         self.etlh.rdh2.bad_regex = {}
-
+    
         lookups = [{'local_id': 'C5604', 'global_id': 'NCI:C5604',
                     'result': 'https://ncit.nci.nih.gov/ncitbrowser/ConceptReport.jsp?dictionary=NCI_Thesaurus&code=C5604'},
                    {'local_id': 'badregexdoesnotmatch', 'global_id': 'MESH:badregexdoesnotmatch',
                     'result': 'https://www.ncbi.nlm.nih.gov/mesh/badregexdoesnotmatch'},
-                   {'local_id': 'Cdiff', 'global_id': 'OMIM:1111', 'result': 'https://www.omim.org/entry/1111'}]
-
+                   {'local_id': 'Cdiff', 'global_id': 'MIM:1111', 'result': 'https://www.omim.org/entry/1111'}] 
+    
         for item in lookups:
             url = self.etlh.get_complete_url_ont(item['local_id'], item['global_id'])
             assert url == item['result']
-
+    
         if self.etlh.rdh2.missing_keys.keys():
             assert 1 == "Should be no missing keys"
         if self.etlh.rdh2.missing_pages.keys():
             assert 1 == "Should be no missing pages"
         for item_name in self.etlh.rdh2.bad_pages.keys():
             # Due to local_id and global not matching we will get a bad pages.
-            assert item_name == "OMIM-None"
+            assert item_name == "MIM-None"  # Updated here
         # mesh fails the regex so make sure we got an error message
-        # we still get a url eror is logged.
+        # we still get a url error is logged.
         for item_name in self.etlh.rdh2.bad_regex.keys():
             assert 1 == self.etlh.rdh2.bad_regex[item_name]
             assert item_name == 'MESH'
