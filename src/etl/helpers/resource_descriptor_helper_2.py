1ad202185667bd23e416d85fa317951be64b8bf5
Christopher Tabone <christabone@gmail.com>
2024-07-12 12:17:40 -0300

    Debugging for strange errors.

--- src/etl/helpers/resource_descriptor_helper_2.py
+++ src/etl/helpers/resource_descriptor_helper_2.py
@@ -325,19 +325,19 @@ class ResourceDescriptorHelper2():
         regex_output = re.match(gid_pattern, identifier_post_processed, re.IGNORECASE)
         if regex_output is None:
             if key not in self.bad_regex:
-                self.logger.warning('Cross Reference identifier did not match Resource Descriptor YAML file gid pattern.')
-                self.logger.warning('Database prefix: %s', db_prefix)
-                self.logger.warning('Identifier: %s', identifier_post_processed)
-                self.logger.warning('gid pattern: %s', gid_pattern)
-                self.logger.warning('page: %s', page)
+                self.logger.info('Cross Reference identifier did not match Resource Descriptor YAML file gid pattern.')
+                self.logger.info('Database prefix: %s', db_prefix)
+                self.logger.info('Identifier: %s', identifier_post_processed)
+                self.logger.info('gid pattern: %s', gid_pattern)
+                self.logger.info('page: %s', page)
                 self.bad_regex[key] = 1
             else:
                 self.bad_regex[key] += 1
-        # if page == "ontology_provided_cross_reference":
-        #     self.logger.info('Processing ontology_provided_cross_reference')
-        #     self.logger.info('DB Prefix: %s', db_prefix)
-        #     self.logger.info('Identifier: %s', identifier)
-        #     self.logger.info('Identifier Processed: %s', identifier_post_processed)
-        #     self.logger.info('GID Pattern: %s', gid_pattern)
-        #     self.logger.info('Regex Match: %s', regex_output)
+        if page == "ontology_provided_cross_reference":
+            self.logger.info('Processing ontology_provided_cross_reference')
+            self.logger.info('DB Prefix: %s', db_prefix)
+            self.logger.info('Identifier: %s', identifier)
+            self.logger.info('Identifier Processed: %s', identifier_post_processed)
+            self.logger.info('GID Pattern: %s', gid_pattern)
+            self.logger.info('Regex Match: %s', regex_output)
         return self.return_url_from_key_value(key, identifier_stripped, alt_page=page)
