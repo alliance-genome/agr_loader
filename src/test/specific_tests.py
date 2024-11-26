420d105a929cd83b488535e53d95ee006e3b50d1
Chris Tabone <christabone@gmail.com>
2024-11-26 16:46:47 -0400

    Update specific_tests.py
    Increased value for Xenbase phenotypes.

--- src/test/specific_tests.py
+++ src/test/specific_tests.py
@@ -304,7 +304,7 @@ def test_phenotype_for_all_species_exists():
                RETURN count(distinct s) AS counter"""
     with Neo4jHelper.run_single_query(query) as result:
         for record in result:
-            assert record["counter"] == 7
+            assert record["counter"] == 8
 
 
 def test_variant_for_expected_species_exists():
